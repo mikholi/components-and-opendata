7e1d2521757c7e63e7054f98804e735a02eaa4b6
mikholi <keranemi@icloud.com>
2024-10-21 16:40:15 +0300

    started orderform

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './ComponentUI'
+import OrderForm from './OrderForm'
 
 
 function App() {
   return (
-    <Header />
+    <><Header /><OrderForm /></>
   )
 }
 
---
+++ src/OrderForm.jsx
@@ -0,0 +1,13 @@
+
+
+function OrderForm() {
+    return (
+        <div className="order-form">
+        <b>Select product</b>
+    </div>
+    )
+    
+}
+
+
+export default OrderForm
