24f769db10e18c661c3b5280b6ceb807fbb9b7b4
mikholi <keranemi@icloud.com>
2024-10-21 16:36:22 +0300

    css styling of header-component

--- src/ComponentUI.jsx
+++ src/ComponentUI.jsx
@@ -4,8 +4,8 @@ import computerimage from './computer.jpg';
 
 function Header() {
     return (
-        <div>
-            <img src={computerimage} alt='computer'></img>
+        <div class='header'>
+            <img className='header-image' src={computerimage} alt='computer'></img>
             <h1>Welcome to the product page!</h1>
         </div>
     )
