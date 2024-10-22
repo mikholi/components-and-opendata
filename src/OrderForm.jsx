ad08eb5c1b370837e59edea2f49f85ea5c7b1e9f
mikholi <keranemi@icloud.com>
2024-10-22 15:51:01 +0300

    models and quantity

--- src/OrderForm.jsx
+++ src/OrderForm.jsx
@@ -1,9 +1,48 @@
+import { useState } from "react"
+
+
+
+
+
+
 
 
 function OrderForm() {
+
+    const products = ['Iphone 11', 'Iphone 12', 'Iphone 13', 'Iphone 14', 'Iphone 15', 'Iphone 16']
+
+    const prices = ['399', '499', '599', '699', '799', '899', '999']
+    
+    const [amount, setAmount] = useState(1);
+    
+    const increaseAmount = () => {
+        setAmount(prevAmount => prevAmount + 1);
+    };
+    
+    const decreaseAmount = () => {
+        setAmount(prevAmount => prevAmount - 1);
+    }
+
     return (
         <div className="order-form">
-        <b>Select product</b>
+            <p></p>
+        <p><b>Select product</b></p>
+        <p></p>
+        <p>Product: </p>
+        <select>
+            <option value="">--Select product --</option>
+            {products.map((product, index)=> (
+                <option key={index} value={product}>
+                    {product}
+                </option>
+            ))}
+        </select>
+        <p>Quantity:</p>
+        <div>
+            <button onClick={increaseAmount}>+</button>
+            <span style={{ margin: '0 10px' }}>{amount}</span>
+            <button onClick={decreaseAmount}>-</button>
+        </div>
     </div>
     )
     
