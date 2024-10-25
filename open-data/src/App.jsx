9abe6ba7dd02598b282009e0a6ce20b3d018e700
mikholi <keranemi@icloud.com>
2024-10-25 13:17:48 +0300

    removed the beweries appearing when starting application

--- open-data/src/App.jsx
+++ open-data/src/App.jsx
@@ -1,44 +1,42 @@
+// src/BreweryList.js
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './App.css';
 
 const BreweryList = () => {
   const [breweries, setBreweries] = useState([]);
-  const [search,setSearch] = useState('');
+  const [search, setSearch] = useState('');
 
-useEffect(() => {
-  axios.get('https://api.openbrewerydb.org/breweries')
-  .then(response => {
-    setBreweries(response.data);
-  })
-  .catch(error => {
-    console.error('Cannot load data', error);
-  });
-}, [])
+  useEffect(() => {
+    if (search) {
+      axios.get(`https://api.openbrewerydb.org/breweries?by_name=${search}`)
+        .then(response => {
+          setBreweries(response.data);
+        })
+        .catch(error => {
+          console.error('Error getting data', error);
+        });
+    } else {
+      setBreweries([]);
+    }
+  }, [search]);
 
-const filteredBreweries = breweries.filter(brewery =>
-  brewery.name.toLowerCase().includes(search.toLocaleLowerCase())
-);
-
-return (
-  <div>
-    <input
-    type="text"
-    placeholder='Search for breweries..'
-    value={search}
-    onChange={e => setSearch(e.target.value)}
-    />
-    <ul>
-      {filteredBreweries.map(brewery => (
-        <li key={brewery.id}>
-          {brewery.name} - {brewery.city}, {brewery.state}
-        </li>
-      ))}
-    </ul>
-  </div>
-)
-
-}
-
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Search breweries..."
+        value={search}
+        onChange={e => setSearch(e.target.value)}
+      />
+      <ul>
+        {breweries.map(brewery => (
+          <li key={brewery.id}>
+            {brewery.name} - {brewery.city}, {brewery.state}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default BreweryList;
