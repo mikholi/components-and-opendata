eb5b6e4a6d55309c3a2990ee517fdd13e0926cd6
mikholi <keranemi@icloud.com>
2024-10-25 11:31:52 +0300

    made start of the open-data funcctions

--- open-data/src/App.jsx
+++ open-data/src/App.jsx
@@ -1,11 +1,22 @@
-import { useState } from 'react'
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './App.css';
 
-function App() {
-  return (
-    <div>
-      testing testing
-    </div>
-  )
+const BreweryList = () => {
+  const [breweries, setBreweries] = useState([]);
+  const [search,setSearch] = useState('');
+
+useEffect(() => {
+  axios.get('https://api.openbrewerydb.org/breweries')
+  .then(response => {
+    setBreweries(response.data);
+  })
+  .catch(error => {
+    console.error('Cannot load data', error);
+  }),
+}, [])
+
 }
-export default App
+
+
+export default App;
