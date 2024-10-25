ff6f05c32ab221f9b4ea4bec85e80bdd4f5fe334
mikholi <keranemi@icloud.com>
2024-10-25 08:41:51 +0300

    deleted unnecessary imports from app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './ComponentUI'
 import OrderForm from './OrderForm'
-import OrderInfo from './OrderInfo'
+
 
 
 function App() {
