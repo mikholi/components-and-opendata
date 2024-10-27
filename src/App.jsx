2c913a1a78de4fa0b2e79ac2359d1788b853c272
mikholi <keranemi@icloud.com>
2024-10-27 08:39:20 +0200

    router started

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Header from './ComponentUI'
-import OrderForm from './OrderForm'
-import OrderInfo from './OrderInfo'
+import App from './main'
+import App from '../open-data'
 
 
 function App() {
-  return (
-    <div>
-      testing
-    </div>
+  return(
+    <Router>
+      <Switch>
+        
+      </Switch>
+    </Router>
   )
 }
 
