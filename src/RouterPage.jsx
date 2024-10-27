b6b02c38362a1c062ef83e9adbce4f02bb3c3aaa
mikholi <keranemi@icloud.com>
2024-10-27 10:20:24 +0200

    trying to do routing

---
+++ src/RouterPage.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import ProductPage from './Assignments/components-and-opendata/src/App';
+import OpenDataPage from './Assignments/components-and-opendata/open-data/src/App';
+
+
+
+export default RouterPage;
